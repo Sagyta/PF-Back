aae8bf60c07b0e602c819d5e44b6ee167630465b
Sagyta <betinarenaudo@gmail.com>
2022-06-16 20:28:15 -0300

    segundo Push

--- api/src/models/Noticias.js
+++ api/src/models/Noticias.js
@@ -4,9 +4,6 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('noticia', {
-    id:{
-      type: DataTypes.INTEGER
-    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
