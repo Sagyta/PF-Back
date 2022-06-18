886072718868fbeac34813a7bd5a7d88f86aae16
Sagyta <betinarenaudo@gmail.com>
2022-06-18 00:13:33 -0300

    fix1

--- api/src/models/Comment.js
+++ api/src/models/Comment.js
@@ -3,11 +3,20 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('noticia', {
-    name: {
+  sequelize.define('comment', {
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+    },
+    comment: {
       type: DataTypes.STRING,
       allowNull: false,
-    },    
+    }, 
+    newId:{
+      type: DataTypes.INTEGER,
+      unique: 'comment'
+    },  
   },
   {
     timestamps: false,
