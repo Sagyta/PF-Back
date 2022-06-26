30f0eb7529f507fd698e5570460074c33eb14ee3
Sagyta <betinarenaudo@gmail.com>
2022-06-26 01:26:45 -0300

    AddAlbum Delete Put

--- api/src/routes/Albums.js
+++ api/src/routes/Albums.js
@@ -1,11 +1,11 @@
 const { Router } = require('express');
-const { getAlbum, postAlbum, getAlbumId } = require('./controllers/Albums');
+const { getAlbum, postAlbum, getAlbumId, putAlbum, deleteAlbum } = require('./controllers/Albums');
 
 const router = Router();
 
 router.get('/', getAlbum)
 router.get('/:id', getAlbumId)
 router.post('/', postAlbum)
-/*router.put('/:id', putCategory)
-router.delete('/:id', deleteCategory) */
+router.put('/:id', putAlbum)
+router.delete('/:id', deleteAlbum) 
 module.exports = router;
