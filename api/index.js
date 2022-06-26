b18874b3c7522a1ef16ac243b7c7fc7810c55a31
Sagyta <betinarenaudo@gmail.com>
2022-06-26 05:16:36 -0300

    Add UserAdmin Category Sport Rol por defecto

--- api/index.js
+++ api/index.js
@@ -2,22 +2,32 @@ const {Role, Sport, Category, User} = require('./src/db.js');
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
-// Syncing all the models at once.
 conn.sync({ force: true }).then(() => { 
   
   server.listen(3001, () => {
-    console.log('Server corriendo'); // eslint-disable-line no-console
+    console.log('Server corriendo'); 
 
     (async function () {
-      await Role.create({name: 'Socio'});
-      await Role.create({name: 'Admin'});
-      await Sport.create({name: 'Futbol'});
-      await Sport.create({name: 'Natacion'});
-      await Sport.create({name: 'Hockey'});
-      await Category.create({name:'Infantil'});
-      await Category.create({name:'Juvenil'});
-      await Category.create({name:'Mayores'});
-      await User.create({
+      const role= await Role.findOne()
+      if(!role){
+        await Role.create({name: 'Socio'});
+        await Role.create({name: 'Admin'});
+      }
+      const category= await Category.findOne()
+      if(!category){
+        await Category.create({name: 'Infantil'});
+        await Category.create({name: 'Juvenil'});
+        await Category.create({name: 'Mayores'});
+      }
+      const sport= await Sport.findOne()
+      if(!sport){
+        await Sport.create({name: 'Futbol'});
+        await Sport.create({name: 'Natacion'});
+        await Sport.create({name: 'Hockey'});
+      }
+      const user= await User.findOne()
+      if(!user){
+        await User.create({
         name: 'Admin',
         surname: 'Henry',
         address: "Bootcamp",
@@ -28,7 +38,8 @@ conn.sync({ force: true }).then(() => {
         dni: "12525452",
         isOlder: true,
         roleId: 2
-      })
-    })();
+        });
+      }
+    })();    
   });
 }); 
