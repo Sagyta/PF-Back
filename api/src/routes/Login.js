5e3c8888c70a226f73010f0f6b6ce3bd951889c3
Sagyta <betinarenaudo@gmail.com>
2022-07-02 19:34:47 -0300

    AddIsBanned

--- api/src/routes/Login.js
+++ api/src/routes/Login.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const { authAdmin } = require('../Middleware/Auth');
+const { authAdmin, isBanned } = require('../Middleware/Auth');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 const { login, members } = require('./controllers/Login');
@@ -7,6 +7,6 @@ const { login, members } = require('./controllers/Login');
 
 const router = Router();
 
-router.post('/',login)
+router.post('/',isBanned,login)
 router.get('/users',authAdmin, members)
 module.exports = router;
