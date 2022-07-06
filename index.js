1d7b8cc8b8f5a87a9bd60e4d4b38e7d9787b1e7f
Marco <marckk63prog@gmail.com>
2022-07-05 23:24:49 -0300

    ready

--- index.js
+++ index.js
@@ -5,8 +5,29 @@ const axios = require('axios');
 
 const {ACCESS_TOKEN} = process.env;
 
-app.get('/', (req,res)=>{
-    res.send('hola que tal')
+app.post('/payment', (req,res)=>{
+   
+    let {payer_email, items} = req.body;
+    const url = "https://api.mercadopago.com/checkout/preferences";
+
+    const body = {
+        payer_email,
+        items,
+        back_urls: {
+            success: "https://prueba-mercado.vercel.app//success",
+            failure: "/failure",
+            pending: "/pending",
+        },
+    };
+
+    const payment = await axios.post(url, body, {
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${process.env.ACCESS_TOKEN}`
+        }
+    });
+    res.send(payment.data);
+    
 });
 
 module.exports = app; 
