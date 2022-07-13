bbfb2b150b967305c544e1806c871e12d3593f97
Marco <marckk63prog@gmail.com>
2022-07-13 04:18:32 -0300

    ready

--- api/src/routes/controllers/postConfirmacion.js
+++ api/src/routes/controllers/postConfirmacion.js
@@ -1,7 +1,20 @@
+const fetch = require('node-fetch');
+
 async function postConfirmacion (req,res){
     let {results} = req.body
-    console.log(results.payments[0].id);
-    console.log(results.items[0].id);
+    const idTickey = results.payments[0].id;
+    const userId = results.items[0].id;
+    const CategorySportId = {CategorySportId: results.items[0].category_id};
+
+
+    const url = 'https://backhenryclub.herokuapp.com/inscription';
+    const data = await fetch(`${url}/${userId}`, {
+        method: 'POST',
+        headers: {
+            "Content-Type":"application/json"
+        },
+        body: JSON.stringify(CategorySportId)
+    });
 
     res.send('hola');
 
